591453cfac82e1a50feacecaf35c4f28a37f8c08
Suttisak Denduangchai <suttisak.lukesp@outlook.com>
2022-10-02 08:30:22 +0700

    Report FHIR Response in JSON

--- fhir_transformer/folders43/processor.py
+++ fhir_transformer/folders43/processor.py
@@ -4,25 +4,31 @@ from fhir_transformer.FHIR.Bundle import Bundle, BundleType
 from fhir_transformer.FHIR.Location import Location
 from fhir_transformer.FHIR.Organization import Organization
 from fhir_transformer.fhir_transformer_config import hospital_name_43folders, hospital_blockchain_address
-from fhir_transformer.folders43.csv_extractor import _open_person_csv
+from fhir_transformer.folders43.csv_extractor import _open_person_csv, _open_provider_csv, _open_drug_opd_csv
+from fhir_transformer.utilities.holder import BundleResult
 from fhir_transformer.utilities.networking import send_bundle
 
-def process(person_csv_path: str):
+
+def process(person_csv_path: str, drug_opd_csv_path: str, provider_csv_path: str) -> list[BundleResult]:
+    results = results = list[BundleResult]()
     personCSV = _open_person_csv(person_csv_path)
+    drugDict = _open_drug_opd_csv(drug_opd_csv_path)  # pid,DrugItem
+    providerDict = _open_provider_csv(provider_csv_path)  # providerId,ProviderItem
     # region GENERATE + SEND ONE ORGANIZATION DATA
     print(f"PREPARE AND SENDING ORGANIZATION {datetime.now()}")
     organization_bundle = Bundle(BundleType.Batch,
                                  [Organization(hospital_name_43folders, hospital_blockchain_address,
                                                personCSV.hospital_code).create_entry()])
-    send_bundle(organization_bundle)
+    results.append(send_bundle(organization_bundle))
     # endregion GENERATE + SEND ONE ORGANIZATION DATA
 
     # region GENERATE + SEND UNIQUE LOCATION DATA
     print(f"PREPARE AND SENDING LOCATION {datetime.now()}")
     locations = dict()
-    for inv_no, bill_trans_item in bill_trans_xml_data.items_dict.items():
+    for inv_no, bill_trans_item in drugDict.items():
         locations[bill_trans_item.station] = Location(station=bill_trans_item.station,
                                                       hospital_blockchain_address=hospital_blockchain_address)
     locations_bundle = Bundle(BundleType.Batch, [entry.create_entry() for entry in list(locations.values())])
     send_bundle(locations_bundle)
     # endregion GENERATE + SEND UNIQUE LOCATION DATA
+    return results
